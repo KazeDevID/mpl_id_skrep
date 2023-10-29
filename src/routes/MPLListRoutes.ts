b5ca71cd726763370b8076068de3d087ed18b9d6
Kaze <101078139+KazeDevID@users.noreply.github.com>
2023-10-29 19:04:27 +0800

    Add files via upload

--- src/routes/MPLListRoutes.ts
+++ src/routes/MPLListRoutes.ts
@@ -1,1 +1,7 @@
+import express, { Express, Router } from "express";
+import MPLController from "../controllers/MPLlist";
+const router = Router()
 
+router.get("/mpl_id", MPLController.getAllMPLList)
+
+export default router
